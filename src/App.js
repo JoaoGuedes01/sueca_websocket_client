9606817d17ddf8e6d2c539b5e0dcc316e1a3f47c
joaoguedes <joaoguedes.cjp@gmail.com>
2023-03-16 21:12:02 +0000

    idk

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './Components/Home/index';
 import Game from './Components/Game/index';
 import PastGames from './Components/PastGames/index';
